4aa96a476493660ca354715236776cb08a98e820
Chad Fowler <chad@livingsocial.com>
2011-12-06 21:33:28 -0500

    tracking down NPE

--- src/protagonizer/github.clj
+++ src/protagonizer/github.clj
@@ -1,15 +1,23 @@
 (ns protagonizer.github
   (:require [clojure.xml :as parser]
-       [clj-http.client :as client]))
+       [clj-http.client :as client]
+       [clj-time.format :as time-format]))
+
 (def root-github-url "https://github.com")
 
 (defn url-for-user [user]
   (str root-github-url "/" user ".atom"))
 
+(defn parse-date [element, name]
+  (let [formatter (time-format/formatters :date-time-noms)
+        date (time-format/parse formatter (first (seq (:content element))))]
+                                {name date}))
+
 (defmulti parse-content :tag)
+(defmethod parse-content :updated [element]
+  (parse-date element :updated))
 (defmethod parse-content :published [element]
-  ; parse the date
-  {:published (:content element)})
+  (parse-date element :published))
 (defmethod parse-content :link [element]
   {:link (:content element)})
 (defmethod parse-content :title [element]
